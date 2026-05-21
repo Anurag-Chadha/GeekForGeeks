d937db8b1606ab151a5f19d5d6d97f9a4d25f031
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-21 22:01:26 +0530

    Update solution.cpp

--- binary-search/floor-in-a-sorted-array/solution.cpp
+++ binary-search/floor-in-a-sorted-array/solution.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int target) {
+        // code here
+        int low=0;int high=arr.size()-1;
+        int mid;
+        int ans=-1;
+        while(low<=high){
+            mid=low+ (high-low)/2;
+            if(arr[mid]<=target)
+            {
+                ans=mid;
+                low = mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
