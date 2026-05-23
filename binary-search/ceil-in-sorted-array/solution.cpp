41864b978e9c4cab39a67bed7f80b21e40805c13
Anurag-Chadha <anuragchadha526@gmail.com>
2026-05-23 09:23:17 +0530

    Update solution.cpp

--- binary-search/ceil-in-sorted-array/solution.cpp
+++ binary-search/ceil-in-sorted-array/solution.cpp
@@ -1,1 +1,18 @@
-
+// User function Template for C++
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int target) {
+        // code here
+        int low=0;int high=arr.size()-1;
+        int mid; int ans=-1;
+        while(low<=high){
+            mid=low+ (high-low)/2;
+            if(arr[mid]>=target) {
+                ans=mid;
+                high= mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+};
